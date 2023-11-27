fc873b65da8179c277c6b076a2b2779e4c363a5f
lamurian <lamuri.aly8@gmail.com>
2023-11-27 12:00:45 +0100

    Set the function to subset or facet based on medication group

--- src/R/visualize.R
+++ src/R/visualize.R
@@ -1,6 +1,6 @@
 # Functions to visualize the data
 
-vizDot <- function(tbl, y, ...) {
+vizDot <- function(tbl, y, groupname = NULL, ...) {
   #' Visualize the Dot Plot
   #'
   #' Visualizing the data frame content as a dot plot
@@ -8,22 +8,32 @@ vizDot <- function(tbl, y, ...) {
   #' @param tbl Time-series data frame containing metrics and statistics of
   #' daily medication claims
   #' @param y The time-varying variable to plot
+  #' @param groupname A character object specifying the medication group name.
+  #' If not provided (`null` by default), all groups are shown as a faceted
+  #' plot.
   #' @inheritDotParams ggplot2::facet_wrap
   #' @return A GGPlot object
   require("ggplot2")
 
   labelname <- dplyr::case_when(
     y == "n_claim" ~ "The number of medication claims",
-    y == "n_patient" ~ "The number of patients prescribed for the medication",
+    y == "n_patient" ~ "The number of patients prescribed for a medication",
     y == "claim2patient" ~ "Medication claim to patient ratio",
     y == "eigen" ~ "Eigenvector centrality",
     y == "pagerank" ~ "Page rank centrality"
   )
 
+  if (!is.null(groupname)) {
+    tbl %<>% subset(.$group == groupname)
+  }
+
   plt <- ggplot(tbl, aes(x = date, y = get(y))) +
     geom_point(alpha = 0.4, size = 0.6) +
-    facet_wrap(~group, scales = "free_y", ...) +
     labs(x = "Year", y = labelname)
+
+  if (is.null(groupname)) {
+    plt <- plt + facet_wrap(~group, scales = "free_y", ...)
+  }
 
   return(plt)
 }
